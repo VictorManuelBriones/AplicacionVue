93afa2e2c80c76b3597dd6ed49cc3d2f441db2cd
Victor Manuel Briones <victorm140399@gmail.com>
2020-06-19 19:06:42 -0500

    Los ciclos en Vue atributos y concatenacion en Vue

---
+++ src/components/List.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <div class="box" v-for="e in elements" v-bind:key="e">
+            {{ e }}            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+          elemets: ["Uno", "Dos", "Tres"]  
+        };
+    }  
+}
+</script>
+
+<style>
+    .box{
+        border: 3px solid #CCC;
+        margin: 5px 0 0 0; 
+    }
+</style>
