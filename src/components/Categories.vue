38c0fb1b11820ded51aae5869092bfc4d060bd9a
Victor Manuel Briones <victorm140399@gmail.com>
2020-06-21 15:07:45 -0500

    Mejorar el componente de carta para el listado de elementos

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,34 +1,31 @@
 <template>
-    <div>
-        <b-list-group>
-            <div v-for="c in categories" v-bind:key="c.id">
-              <b-list-group-item>
-                <router-link :to="'/category' + c.id + '/caregory'"><router-link>
-                    {{ c.title }}
-              </b-list-group-item>
-            </div>
-        </b-list-group>          
-    </div>
+  <div>
+    <h5>Categorías</h5>
+    <b-list-group>
+      <b-list-group-item button v-for="c in categories" v-bind:key="c.id">
+        <router-link :to=" '/category/' + c.id +'/elements'">{{ c.title }}</router-link>
+      </b-list-group-item>
+    </b-list-group>
+  </div>
 </template>
+
 
 <script>
 export default {
-
-    created(){
-        this.finAll()
-    },
-
-    data() {
-        return {
-            caregories: []  
-        };
-    }, 
-    //methods: {
-        //findAll: function(){
-            //fech('http://localhost:8000/api/category/?format=json')
-                //.then(res => res.json())
-                //.then(res => this.categories = res)
-        //}
-    //}, 
-}
+  created() {
+    this.findAll();
+  },
+  data() {
+    return {
+      categories: []
+    };
+  },
+  methods: {
+    findAll: function() {
+      fetch("http://localhost:8000/api/category/?format=json")
+        .then(res => res.json())
+        .then(res => (this.categories = res));
+    }
+  }
+};
 </script>
--- src/components/Types.vue
+++ src/components/Types.vue
@@ -1,33 +1,31 @@
 <template>
-    <div>
-        <b-list-group>
-            <div v-for="t in types" v-bind:key="t.id">
-                <b-list-group-item button>
-                <router-link :to="'/type/' + t.id + '/element'"><router-link>{{ t.title }}</router-link>
-                </b-list-group-item>
-            </div>
-        </b-list-group>          
-    </div>
+  <div>
+    <h5>Tipos</h5>
+    <b-list-group>
+      <b-list-group-item button v-for="t in types" v-bind:key="t.id">
+        <router-link :to=" '/type/' + t.id +'/elements'">{{ t.title }}</router-link>
+      </b-list-group-item>
+    </b-list-group>
+  </div>
 </template>
+
 
 <script>
 export default {
-
-    created(){
-        this.finAll()
-    },
-
-    data() {
-        return {
-            types: []  
-        };
-    }, 
-    //methods: {
-        //findAll: function(){
-            //fech('http://localhost:8000/api/type/?format=json')
-                //.then(res => res.json())
-                //.then(res => this.types = res)
-        //}
-    //}, 
-}
+  created() {
+    this.findAll();
+  },
+  data() {
+    return {
+      types: []
+    };
+  },
+  methods: {
+    findAll: function() {
+      fetch("http://localhost:8000/api/type/?format=json")
+        .then(res => res.json())
+        .then(res => (this.types = res));
+    }
+  }
+};
 </script>
